c88bad88a107bfab8bbc25e08bd7cd4bae1f39c3
Vale <oirelav95@gmail.com>
2024-01-31 16:14:06 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script>
+import HelloWorld from './components/HelloWorld.vue';
+
+export default {
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<template>
+  <HelloWorld />
+</template>
+
+<style lang="scss">
+@use '../src/styles/general.scss' as *;
+@use '../src/styles/partials/mixins' as *;
+@use '../src/styles/partials/variables' as *;
+</style>
