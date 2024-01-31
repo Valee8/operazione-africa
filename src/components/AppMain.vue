ef9524f8fca1cf62b7c451c56ab79b03595cb70c
Vale <oirelav95@gmail.com>
2024-01-31 16:26:17 +0100

    create component AppMain

---
+++ src/components/AppMain.vue
@@ -0,0 +1,23 @@
+<script >
+export default {
+    name: "AppMain",
+    data() {
+        return {
+            message: "Main"
+        }
+    }
+}
+</script>
+
+<template>
+    <h1>
+        {{ message }}
+    </h1>
+</template>
+
+<style lang="scss" scoped>
+h1 {
+    color: red;
+    text-align: center;
+}
+</style>
