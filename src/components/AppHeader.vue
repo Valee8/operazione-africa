9d4375c266a86e789aa54be2fdb9ff6393743d0e
Vale <oirelav95@gmail.com>
2024-01-31 16:25:48 +0100

    create component AppHeader

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,23 @@
+<script >
+export default {
+  name: "AppHeader",
+  data() {
+    return {
+      message: "Header"
+    }
+  }
+}
+</script>
+
+<template>
+  <h1>
+    {{ message }}
+  </h1>
+</template>
+
+<style lang="scss" scoped>
+h1 {
+  color: red;
+  text-align: center;
+}
+</style>
