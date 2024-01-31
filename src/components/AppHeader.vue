41e0464e280c5d1f8b37cf85e3596a4b5d940142
Vale <oirelav95@gmail.com>
2024-01-31 17:40:57 +0100

    add header and logo in AppHeader

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -10,14 +10,32 @@ export default {
 </script>
 
 <template>
-  <h1>
-    {{ message }}
-  </h1>
+  <header>
+    <div class="container">
+      <div class="logo">
+        <img src="/img/logo.jpg" alt="Logo Operazione Africa" class="logo-img" width="23" height="23">
+        <span class="logo-text">
+          Operazione Africa
+        </span>
+      </div>
+    </div>
+  </header>
 </template>
 
 <style lang="scss" scoped>
-h1 {
-  color: red;
-  text-align: center;
+header {
+  padding-top: 28px;
+
+  .logo {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    .logo-text {
+      font-family: 'Plus Jakarta Sans', sans-serif;
+      font-size: 1.25rem;
+      font-weight: 700;
+    }
+  }
 }
 </style>
