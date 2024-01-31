4ace299a3daa5b50e36ce36e68bb92455e1808a8
Vale <oirelav95@gmail.com>
2024-01-31 16:25:41 +0100

    create component AppFooter

---
+++ src/components/AppFooter.vue
@@ -0,0 +1,23 @@
+<script >
+export default {
+    name: "AppFooter",
+    data() {
+        return {
+            message: "Footer"
+        }
+    }
+}
+</script>
+
+<template>
+    <h1>
+        {{ message }}
+    </h1>
+</template>
+
+<style lang="scss" scoped>
+h1 {
+    color: red;
+    text-align: center;
+}
+</style>
