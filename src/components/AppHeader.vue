a1596ba16bda23c503ffb41d44cd6ee80dcfec65
Vale <oirelav95@gmail.com>
2024-02-01 12:34:17 +0100

    add css for navbar and header

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,6 +1,13 @@
 <script >
+
+import AppNavbar from './AppNavbar.vue';
+
+
 export default {
   name: "AppHeader",
+  components: {
+    AppNavbar
+  },
   data() {
     return {
       message: "Header"
@@ -11,6 +18,9 @@ export default {
 
 <template>
   <header>
+    <AppNavbar />
+
+    <!-- <div class="bg-green"></div> -->
     <div class="container">
       <div class="logo">
         <img src="/img/logo.jpg" alt="Logo Operazione Africa" class="logo-img" width="23" height="23">
@@ -18,23 +28,69 @@ export default {
           Operazione Africa
         </span>
       </div>
+
+      <div class="button donate">
+        <a href="/">
+          Donate <i class="fa-solid fa-arrow-right-long"></i>
+        </a>
+      </div>
     </div>
   </header>
 </template>
 
 <style lang="scss" scoped>
 header {
-  padding-top: 28px;
+  //padding-top: 48px;
+  min-height: 800px;
 
-  .logo {
+  // .prova {
+  //   position: relative;
+  //   z-index: 20;
+  // }
+
+  .container {
     display: flex;
-    justify-content: center;
+    justify-content: space-between;
     align-items: center;
+    position: absolute;
+    top: 28px;
+    left: 50%;
+    transform: translateX(-50%);
+    z-index: 1;
 
-    .logo-text {
-      font-family: 'Plus Jakarta Sans', sans-serif;
+    .logo {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+
+      .logo-text {
+        font-family: 'Plus Jakarta Sans', sans-serif;
+        font-size: 1.25rem;
+        font-weight: 700;
+        color: #3A3A3A;
+      }
+    }
+
+    .donate {
+      width: 199px;
+      height: 59px;
+      line-height: 59px;
       font-size: 1.25rem;
-      font-weight: 700;
+      font-family: 'Poppins', sans-serif;
+      position: relative;
+
+      a {
+        color: #fff;
+        font-weight: 700;
+
+        svg {
+          font-weight: normal;
+          font-size: 0.9rem;
+          padding-left: 10px;
+          vertical-align: middle;
+        }
+
+      }
     }
   }
 }
