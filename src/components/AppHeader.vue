fd7c4fce0134109587ffc762a76bf0aa9d23c3db
Vale <oirelav95@gmail.com>
2024-02-01 13:33:16 +0100

    add content in header

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -20,19 +20,36 @@ export default {
   <header>
     <AppNavbar />
 
-    <!-- <div class="bg-green"></div> -->
-    <div class="container">
+    <div class="container top">
       <div class="logo">
-        <img src="/img/logo.jpg" alt="Logo Operazione Africa" class="logo-img" width="23" height="23">
+        <img src="/img/logo.png" alt="Logo Operazione Africa" class="logo-img" width="23" height="23">
         <span class="logo-text">
           Operazione Africa
         </span>
       </div>
 
-      <div class="button donate">
-        <a href="/">
-          Donate <i class="fa-solid fa-arrow-right-long"></i>
-        </a>
+      <a href="/" class="button donate">
+        Donate <i class="fa-solid fa-arrow-right-long"></i>
+      </a>
+    </div>
+
+    <div class="container">
+      <div class="header-content">
+
+        <div class="content-left">
+          <h6>
+            Operazione Africa
+          </h6>
+
+          <h1>
+            Being Part Of Aid Foundation Is A Way To Share
+          </h1>
+
+          <p>
+            Lorem ipsum dolor sit amet consectetur. Ultrices ultrices pulvinar maecenas neque. Sit tellus nunc enim cursus
+            dolor ultrices sit ullamcorper aliqueorem aliquet.
+          </p>
+        </div>
       </div>
     </div>
   </header>
@@ -40,46 +57,45 @@ export default {
 
 <style lang="scss" scoped>
 header {
-  //padding-top: 48px;
   min-height: 800px;
 
-  // .prova {
-  //   position: relative;
-  //   z-index: 20;
-  // }
-
   .container {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    position: absolute;
-    top: 28px;
-    left: 50%;
-    transform: translateX(-50%);
-    z-index: 1;
 
-    .logo {
-      display: flex;
-      justify-content: center;
-      align-items: center;
+    &:not(.top) {
+      padding-top: 100px;
 
-      .logo-text {
-        font-family: 'Plus Jakarta Sans', sans-serif;
-        font-size: 1.25rem;
-        font-weight: 700;
-        color: #3A3A3A;
-      }
     }
 
-    .donate {
-      width: 199px;
-      height: 59px;
-      line-height: 59px;
-      font-size: 1.25rem;
-      font-family: 'Poppins', sans-serif;
-      position: relative;
+    &.top {
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      position: absolute;
+      top: 28px;
+      left: 50%;
+      transform: translateX(-50%);
+      z-index: 1;
 
-      a {
+      .logo {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+
+        .logo-text {
+          font-family: 'Plus Jakarta Sans', sans-serif;
+          font-size: 1.25rem;
+          font-weight: 700;
+          color: #3A3A3A;
+        }
+      }
+
+      .donate {
+        width: 199px;
+        height: 59px;
+        line-height: 59px;
+        font-size: 1.25rem;
+        font-family: 'Poppins', sans-serif;
+        position: relative;
         color: #fff;
         font-weight: 700;
 
@@ -88,8 +104,34 @@ header {
           font-size: 0.9rem;
           padding-left: 10px;
           vertical-align: middle;
+
+        }
+      }
+    }
+
+    .header-content {
+      .content-left {
+
+
+        h6 {
+          font-weight: 500;
+          font-size: 1.25rem;
         }
 
+        h1 {
+          padding: 30px 0;
+          font-weight: 700;
+          font-size: 3.75rem;
+          width: 505px;
+        }
+
+        p {
+          font-weight: 400;
+          font-size: 1.19rem;
+          line-height: 33px;
+          width: 611px;
+
+        }
       }
     }
   }
