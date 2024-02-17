32bc16b7862d6dc4bcdaa3e3a7652999076665d1
Vale <oirelav95@gmail.com>
2024-02-17 12:12:45 +0100

    create component AboutUs

---
+++ src/components/AboutUsSection.vue
@@ -0,0 +1,24 @@
+<script >
+
+export default {
+    name: "AboutUsSection"
+}
+</script>
+
+<template>
+    <section>
+        <h1>
+            About US
+        </h1>
+    </section>
+</template>
+
+<style lang="scss" scoped>
+@use '../src/styles/general.scss' as *;
+@use '../src/styles/partials/mixins' as *;
+@use '../src/styles/partials/variables' as *;
+
+h1 {
+    text-align: center;
+}
+</style>
