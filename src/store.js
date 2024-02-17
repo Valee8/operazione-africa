653c71be7cb8aeb40459c511026fbb3ff06bc0c7
Vale <oirelav95@gmail.com>
2024-02-17 12:12:33 +0100

    create file store.js

---
+++ src/store.js
@@ -0,0 +1,31 @@
+// import { reactive } from 'vue';
+
+// export const store = reactive({
+//     linksNav: [
+//         {
+//             text: "Home",
+//             name: "home",
+//             active: true
+//         },
+//         {
+//             text: "About Us",
+//             name: "about-us",
+//             active: false
+//         },
+//         {
+//             text: "Services",
+//             href: "/",
+//             active: false
+//         },
+//         {
+//             text: "Teams",
+//             href: "/",
+//             active: false
+//         },
+//         {
+//             text: "Get in Touch",
+//             href: "/",
+//             active: false
+//         }
+//     ]
+// });
