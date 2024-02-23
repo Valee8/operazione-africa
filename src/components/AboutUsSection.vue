a320fbce716e40b98462682d3fbcd2b52f1e708f
Vale <oirelav95@gmail.com>
2024-02-23 16:39:16 +0100

    add first about us section

--- src/components/AboutUsSection.vue
+++ src/components/AboutUsSection.vue
@@ -1,15 +1,47 @@
 <script >
 
 export default {
-    name: "AboutUsSection"
+    name: "AboutUsSection",
+    data() {
+        return {
+            firstSectionLinks: [
+                {
+                    icon: "img/about-us-icon1.png",
+                    title: "Great team work",
+                    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur tempor nunc non neque euismod porttitor. Nullam lacus est, tincidunt eget sapien eget, maximus convallis massa. Curabitur quis tellus a tortor egestas viverra.",
+                    width: 84
+                },
+                {
+                    icon: "img/about-us-icon2.png",
+                    title: "Our vision",
+                    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam..",
+                    width: 86
+                },
+                {
+                    icon: "img/about-us-icon3.png",
+                    title: "Our mission",
+                    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam..",
+                    width: 85
+                }
+            ]
+        }
+    }
 }
 </script>
 
 <template>
-    <section>
-        <h1>
-            About US
-        </h1>
+    <section class="first-section">
+        <div class="container">
+            <div v-for="(links, index) in firstSectionLinks" :key="index" class="cards">
+                <img :src="links.icon" :alt="'Immagine ' + links.title" class="card-image" :width="links.width" height="86">
+                <h3>
+                    {{ links.title }}
+                </h3>
+                <p>
+                    {{ links.description }}
+                </p>
+            </div>
+        </div>
     </section>
 </template>
 
@@ -18,7 +50,35 @@ export default {
 @use '../src/styles/partials/mixins' as *;
 @use '../src/styles/partials/variables' as *;
 
-h1 {
-    text-align: center;
+.first-section {
+    padding: 100px 0;
+
+    .container {
+        display: flex;
+        justify-content: space-between;
+        align-items: flex-start;
+        gap: 50px;
+
+
+        .cards {
+            text-align: center;
+
+            .card-image {
+                padding: 10px 0;
+            }
+
+            h3 {
+                font-size: 3rem;
+                padding: 20px 0;
+                font-weight: 600;
+            }
+
+            p {
+                font-size: 1.125rem;
+                color: #878787;
+                line-height: 36px;
+            }
+        }
+    }
 }
 </style>
